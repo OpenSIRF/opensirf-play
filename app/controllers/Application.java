ed4dbb14bf5285613c955bcf7393010f049fc4e3
Phillip Viana <pviana@us.ibm.com>
2016-03-15 23:03:09 -0500

    Support for PO editing and creation.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,20 +1,39 @@
 package controllers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.ClientResponse;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
+
+import play.mvc.BodyParser;
+import play.mvc.Http;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Http.RequestBody;
+
 import org.opensirf.audit.AuditLogReference;
 import org.opensirf.audit.PreservationObjectAuditLog;
 import org.opensirf.catalog.SIRFCatalog;
+import org.opensirf.jaxrs.api.ObjectApi;
 import org.opensirf.jaxrs.config.SIRFConfiguration;
 import org.opensirf.jaxrs.config.SIRFConfigurationMarshaller;
 import org.opensirf.jaxrs.config.SIRFConfigurationUnmarshaller;
@@ -32,8 +51,8 @@ import org.opensirf.obj.PreservationObjectVersionIdentifier;
 import org.opensirf.obj.RelatedObjectReference;
 import org.opensirf.obj.RelatedObjects;
 import org.opensirf.obj.Retention;
-import org.opensirf.storage.StorageContainerStrategy;
-import org.opensirf.storage.StrategyFactory;
+import org.opensirf.jaxrs.api.StorageContainerStrategy;
+import org.opensirf.jaxrs.api.StrategyFactory;
 
 import play.data.Form;
 import play.mvc.Controller;
@@ -55,7 +74,6 @@ import views.html.setupTemplate;
 public class Application extends Controller {
 
 	public Result dynamicFormElements(String category, int index, int subIndex) {
-		System.out.println("" + category + " " + index + " " + subIndex);
 		switch (category) {
 		case "Extension":
 			return ok(extensionTemplate.render(null, index));
@@ -78,23 +96,83 @@ public class Application extends Controller {
 		}
 	}
 
+	@BodyParser.Of(BodyParser.MultipartFormData.class)
 	public Result addPreservationObjectInformation() {
-		Form<PreservationObjectInformation> formPreservationObjectInformation = Form.form(PreservationObjectInformation.class)
-				.bindFromRequest();
+		Form<PreservationObjectInformation> formPreservationObjectInformation = Form.form(
+				PreservationObjectInformation.class).bindFromRequest();
+
 		PreservationObjectInformation poi = formPreservationObjectInformation.get();
-		return ok(preservationObjectInformationTemplate.render(Form.form(PreservationObjectInformation.class), poi));
+		
+		RequestBody body = request().body();
+		
+		for(FilePart fp: body.asMultipartFormData().getFiles()) {
+			if(fp.getKey().equals("inputStream")) {
+				try {
+					SIRFConfiguration conf = getConfig();
+					
+				    FileDataBodyPart filePart = new FileDataBodyPart("inputstream", fp.getFile());
+				    FormDataBodyPart poiPart = new FormDataBodyPart("poi", poi,
+				    		MediaType.APPLICATION_JSON_TYPE);
+					
+					String endpoint = new String(Files.readAllBytes(Paths.get(SIRFConfiguration.
+							SIRF_DEFAULT_DIRECTORY + "endpoint")));
+					Client client = ClientBuilder.newBuilder().
+							register(MultiPartFeature.class).
+							build();
+
+					WebTarget resource = client.target("http://" + endpoint + "/sirf/container/" +
+							conf.getContainerConfiguration().getContainerName() + "/po");
+					
+					MultiPart multipartEntity = new FormDataMultiPart()
+				    		.bodyPart(poiPart).bodyPart(filePart);
+					
+					Response response = resource.request().post(
+							Entity.entity(multipartEntity, multipartEntity.getMediaType()));
+					
+					String output = response.readEntity(String.class);
+					
+					System.out.println("OpenSIRF JAX-RS Response:");
+					System.out.println(output);
+				} catch(IOException ioe) {
+					ioe.printStackTrace();
+				}
+			}
+		}
+
+		return ok(preservationObjectInformationTemplate.render(Form.form(
+				PreservationObjectInformation.class), poi));
+	}
+	
+	public Result savePreservationObjectInformation() {
+		Form<PreservationObjectInformation> formPreservationObjectInformation = Form.form(
+				PreservationObjectInformation.class).bindFromRequest();
+
+		PreservationObjectInformation poi = formPreservationObjectInformation.get();
+		
+		poi.setVersionIdentifierUUID(poi.getObjectIdentifiers().get(0).getObjectVersionIdentifier().
+				getObjectIdentifierValue());
+		
+		System.out.println("updating poi = " + poi.getVersionIdentifierUUID());
+		
+		SIRFConfiguration conf = getConfig();
+		StorageContainerStrategy strat = StrategyFactory.createStrategy(conf);
+		SIRFCatalog c = strat.getCatalog();
+		c.getSirfObjects().remove(poi.getVersionIdentifierUUID());
+		c.getSirfObjects().add(poi);
+		strat.pushCatalog(c);
+		
+		return catalog();
 	}
 	
 	public Result createPreservationObjectInformation() {
-		Form<PreservationObjectInformation> formPreservationObjectInformation = Form.form(PreservationObjectInformation.class)
-				.bindFromRequest();
-		PreservationObjectInformation poi = formPreservationObjectInformation.get();
-		return ok(preservationObjectInformationTemplate.render(Form.form(PreservationObjectInformation.class), poi));
+		return ok(preservationObjectInformationTemplate.render(
+				Form.form(PreservationObjectInformation.class), null));
 	}
 
 	public Result saveSetup(String endpoint) {
 		try {
-			Files.write(Paths.get(SIRFConfiguration.SIRF_DEFAULT_DIRECTORY + "endpoint"), endpoint.getBytes());
+			Files.write(Paths.get(SIRFConfiguration.SIRF_DEFAULT_DIRECTORY + "endpoint"),
+					endpoint.getBytes());
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
@@ -103,12 +181,32 @@ public class Application extends Controller {
 	}
 
 	public Result editPreservationObject(String uuid) {
-		// Load config here
-		// TODO
-		return null;
+		System.out.println("UUID=" + uuid);
+		SIRFConfiguration conf = getConfig();
+		StorageContainerStrategy strat = StrategyFactory.createStrategy(conf);
+		PreservationObjectInformation poi = strat.getCatalog().getSirfObjects().get(uuid);
+		
+		return ok(preservationObjectInformationTemplate.render(Form.form(
+				PreservationObjectInformation.class), poi));
 	}
 
-	public Result addCatalog() {
+	public Result downloadPreservationObject(String uuid) {
+		try {
+			SIRFConfiguration conf = getConfig();
+			StorageContainerStrategy strat = StrategyFactory.createStrategy(conf);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			strat.getPreservationObjectStreamingOutput(uuid).write(baos);
+			
+			response().setHeader("Content-disposition","attachment; filename=" + uuid);
+			
+			return ok(baos.toByteArray());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Result saveCatalog() {
 		Form<SIRFCatalog> formCatalog = Form.form(SIRFCatalog.class).bindFromRequest();
 		SIRFCatalog c = formCatalog.get();
 		
@@ -116,7 +214,7 @@ public class Application extends Controller {
 		StorageContainerStrategy strat = StrategyFactory.createStrategy(conf);
 		strat.pushCatalog(c);
 		
-		return ok(catalogTemplate.render(Form.form(SIRFCatalog.class), c));
+		return catalog();
 	}
 
 	public Result catalog() {
@@ -128,7 +226,8 @@ public class Application extends Controller {
 	
 	private SIRFConfiguration getConfig() {
 		try {
-			String endpoint = new String(Files.readAllBytes(Paths.get(SIRFConfiguration.SIRF_DEFAULT_DIRECTORY + "endpoint")));
+			String endpoint = new String(Files.readAllBytes(Paths.get(
+					SIRFConfiguration.SIRF_DEFAULT_DIRECTORY + "endpoint")));
 			Client client = ClientBuilder.newClient();
 			WebTarget resource = client.target("http://" + endpoint + "/sirf/config");
 			Builder request = resource.request();
@@ -142,76 +241,12 @@ public class Application extends Controller {
 		}
 	}
 
-//	private PreservationObjectInformation po() {
-//		PreservationObjectInformation poi = new PreservationObjectInformation();
-//		poi.setObjectRetention(new Retention("retType", "retValue"));
-//		poi.setPackagingFormat(new PackagingFormat("packFormat"));
-//
-//		DigestInformation di = new DigestInformation("do1", "da1", "dv1");
-//		poi.setObjectFixity(new FixityInformation(di));
-//
-//		Extension e1 = new Extension("org1", "desc1");
-//		Extension e2 = new Extension("org2", "desc2");
-//
-//		e1.getObjectExtensionPairs().add(new ExtensionPair("key1", "value1"));
-//		e1.getObjectExtensionPairs().add(new ExtensionPair("key2", "value2"));
-//		e1.getObjectExtensionPairs().add(new ExtensionPair("key3", "value3"));
-//
-//		e2.getObjectExtensionPairs().add(new ExtensionPair("key4", "value4"));
-//		e2.getObjectExtensionPairs().add(new ExtensionPair("key5", "value5"));
-//		e2.getObjectExtensionPairs().add(new ExtensionPair("key6", "value6"));
-//		e2.getObjectExtensionPairs().add(new ExtensionPair("key7", "value7"));
-//
-//		poi.getObjectExtension().add(e1);
-//		poi.getObjectExtension().add(e2);
-//
-//		PreservationObjectIdentifier poid = new PreservationObjectIdentifier();
-//		poid.setObjectLogicalIdentifier(
-//				new PreservationObjectLogicalIdentifier("logicalType1", "logicalLocale1", "logicalValue1"));
-//		poid.setObjectVersionIdentifier(
-//				new PreservationObjectVersionIdentifier("versionType1", "versionLocale1", "versionValue1"));
-//		poid.setObjectParentIdentifier(new PreservationObjectParentIdentifier("parentType1", "parentLocale1", "parentValue1"));
-//		poid.putObjectName(new PreservationObjectName("nameType1", "nameLocale1", "nameValue1"));
-//		poid.putObjectName(new PreservationObjectName("nameType2", "nameLocale2", "nameValue2"));
-//		poid.putObjectName(new PreservationObjectName("nameType3", "nameLocale3", "nameValue3"));
-//		poi.addObjectIdentifier(poid);
-//
-//		poid = new PreservationObjectIdentifier();
-//		poid.setObjectLogicalIdentifier(
-//				new PreservationObjectLogicalIdentifier("logicalType2", "logicalLocale2", "logicalValue2"));
-//		poid.setObjectVersionIdentifier(
-//				new PreservationObjectVersionIdentifier("versionType2", "versionLocale2", "versionValue2"));
-//		poid.setObjectParentIdentifier(new PreservationObjectParentIdentifier("parentType2", "parentLocale2", "parentValue2"));
-//		poid.putObjectName(new PreservationObjectName("nameType4", "nameLocale4", "nameValue4"));
-//		poid.putObjectName(new PreservationObjectName("nameType5", "nameLocale5", "nameValue5"));
-//		poi.addObjectIdentifier(poid);
-//
-//		ArrayList<RelatedObjects> relatedObjects = new ArrayList<RelatedObjects>();
-//		RelatedObjects relatedObject = new RelatedObjects();
-//		relatedObject.setObjectRelatedObjectReference(new RelatedObjectReference("type", "role", "value"));
-//		relatedObjects.add(relatedObject);
-//		poi.setObjectRelatedObjects(relatedObjects);
-//
-//		ArrayList<PreservationObjectAuditLog> auditLogs = new ArrayList<PreservationObjectAuditLog>();
-//		PreservationObjectAuditLog auditLog = new PreservationObjectAuditLog();
-//		auditLog.setObjectAuditLogReference(new AuditLogReference("type", "role", "value"));
-//		auditLogs.add(auditLog);
-//		poi.setObjectAuditLog(auditLogs);
-//
-//		return poi;
-//	}
-//
-//	public Result poiUnitTest() {
-//		PreservationObjectInformation poi = po();
-//
-//		return ok(preservationObjectInformationTemplate.render(Form.form(PreservationObjectInformation.class), poi));
-//	}
-
 	public Result setup() {
 		String s;
 
 		try {
-			s = new String(Files.readAllBytes(Paths.get(SIRFConfiguration.SIRF_DEFAULT_DIRECTORY + "endpoint")));
+			s = new String(Files.readAllBytes(Paths.get(SIRFConfiguration.SIRF_DEFAULT_DIRECTORY +
+					"endpoint")));
 		} catch (IOException e) {
 			s = "";
 		}
